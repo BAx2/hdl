eb1a29abd587508498147ba4b3747f889ed4a26b
Nick Petrovsky <nick@petrovsky.eu>
2021-03-28 20:40:47 +0300

    Fix spelling.

--- library/axi_sevenseg/axi_sevenseg_ip.tcl
+++ library/axi_sevenseg/axi_sevenseg_ip.tcl
@@ -4,7 +4,7 @@ source ../scripts/adi_env.tcl
 source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
 
 adi_ip_create axi_sevenseg
-adi_ip_files  axi_puf [list \
+adi_ip_files  axi_sevenseg [list \
   "hdl/SevenSegController_v1_0.v" \
   "hdl/SevenSegController_v1_0_S00_AXI.v" \
   "hdl/binary_to_bcd.v"
