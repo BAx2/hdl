2cd7df47f62b1f89e64f2deeb382a48ce727587f
anton <antosh-ka2000@list.ru>
2021-10-19 14:30:45 +0300

    added move bitstream and hwh files to separate dir

--- projects/hdmi-out/pynq-z2/system_project.tcl
+++ projects/hdmi-out/pynq-z2/system_project.tcl
@@ -3,10 +3,24 @@ source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-adi_project hdmi-out_pynq-z2
+set project_name hdmi-out_pynq-z2
+set output_dir $project_name.output_files
+
+# adi_project hdmi-out_pynq-z2
+adi_project $project_name
 adi_project_files hdmi-out [list \
   "system_top.v" \
   "system_constr.xdc" \
   "placer_constr.xdc"]
 
-adi_project_run hdmi-out_pynq-z2
+# adi_project_run hdmi-out_pynq-z2
+adi_project_run $project_name
+
+
+exec mkdir -p $output_dir
+exec cp $project_name.sdk/system_top.xsa \
+        $output_dir/$project_name.xsa
+exec cp $project_name.gen/sources_1/bd/system/hw_handoff/system.hwh \
+        $output_dir/$project_name.hwh
+exec cp $project_name.runs/impl_1/system_top.bit \
+        $output_dir/$project_name.bit
