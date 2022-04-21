528e0640d91019c5e34ddeadd624a34a0840ae95
Anton Burak <burakaa@okbtsp.com>
2022-04-21 12:36:35 +0300

    Add interfaces to rgb2dvi

--- library/rgb2dvi/rgb2dvi_ip.tcl
+++ library/rgb2dvi/rgb2dvi_ip.tcl
@@ -16,8 +16,30 @@ adi_ip_files  rgb2dvi [list \
 "src/SyncAsync.vhd" \
 "src/TMDS_Encoder.vhd"]
 
-adi_ip_properties_lite axi_rgbled
+adi_ip_properties_lite rgb2dvi
 
 set cc [ipx::current_core]
+
+adi_add_bus "RGB" "slave" \
+    "xilinx.com:interface:vid_io_rtl:1.0" \
+    "xilinx.com:interface:vid_io:1.0" \
+    { \
+        { "vid_pData"  "DATA" } \
+        { "vid_pVDE"   "ACTIVE_VIDEO" } \
+        { "vid_pHSync" "HSYNC" } \
+        { "vid_pVSync" "VSYNC" } \
+    }
+
+adi_add_bus_clock "PixelClk" "RGB" "aRst"
+
+adi_add_bus "TMDS" "master" \
+    "analog.com:interface:tmds_rtl:1.0" \
+    "analog.com:interface:tmds:1.0" \
+    { \
+        { "TMDS_Clk_p"  "clk_p" } \
+        { "TMDS_Clk_n"  "clk_n" } \
+        { "TMDS_Data_p" "data_p" } \
+        { "TMDS_Data_n" "data_n" } \
+    }
 
 ipx::save_core $cc
